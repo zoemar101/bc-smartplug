9812e37604caa70bcb2c77f75b512575506ade79
zoemar101 <zoemar.baluarte@gmail.com>
2018-04-20 20:12:47 +0800

    modifying template

--- static/js/features/device-js.js
+++ static/js/features/device-js.js
@@ -2,8 +2,19 @@
  * Created by JOMAR T. BALUARTE on 17 Apr 2018.
  */
 $(function() {
-
     $('#change-stat').change(function() {
       $('#status').html($(this).prop('checked'))
     })
+
   })
+
+function changestat () {
+    $.ajax({
+        url:'/transactions/new',
+        type:'',
+        dataType:'POST',
+
+        success: function(response){}
+
+    })
+}
